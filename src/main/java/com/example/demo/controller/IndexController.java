2f000fcb8605bfd5e3c0ac286272638a1b53b434
aelimited <cloim09@gmail.com>
2024-04-25 14:31:43 +0900

    docker test

---
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -0,0 +1,23 @@
+package com.example.demo.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api")
+@RestController
+public class IndexController {
+
+    //GET http://localhost:8080/api/index
+    @GetMapping("index")
+    public ResponseEntity<String> index() {
+        return ResponseEntity.ok().body("hello");
+    }
+
+    @GetMapping("welcome")
+    public ResponseEntity<String> welcome() {
+        return ResponseEntity.ok().body("welcome");
+    }
+
+}
